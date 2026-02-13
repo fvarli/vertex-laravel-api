6702f140b8464463b0ea4bee9d47aebe8161b899
Ferzender Varli <fvarlii@gmail.com>
2026-02-14 02:15:26 +0300

    test(infra): isolate testing storage path and disable file logging in phpunit

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,17 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\File;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $storagePath = sys_get_temp_dir() . '/vertex-laravel-api-testing-storage';
+
+        File::ensureDirectoryExists($storagePath);
+        $this->app->useStoragePath($storagePath);
+    }
 }
